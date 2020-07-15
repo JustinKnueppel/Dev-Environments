1e83254307b2aff5e91938d372d140917e7e5bac
Justin <justinknueppel@gmail.com>
2020-07-15 16:52:04 -0400

    Finish script that copies directory for you

--- dev-env.sh
+++ dev-env.sh
@@ -1,25 +1,50 @@
 #! /usr/bin/env bash
 # saner programming env: these switches turn some bugs into errors
 set -o errexit -o pipefail -o noclobber -o nounset
-DEVELOPMENT_ENVIRONMENT=$(pwd)
 
-if [ $# -ne 1 ]; then
-  echo "Usage: dev-env <language>"
+# Get path of script
+dev_dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+
+# Ensure script called with language and path
+if [ $# -ne 2 ]; then
+  echo "Usage: dev-env <language> <path>"
   exit 1
 fi
 
+# Map language to directory
 language=$1
 dir=""
 case $language in
   python)
-  dir="$DEVELOPMENT_ENVIRONMENT/python"
+  dir="$dev_dir/python"
   ;;
   go)
-  dir="$DEVELOPMENT_ENVIRONMENT/go"
+  dir="$dev_dir/go"
   ;;
   node)
-  dir="$DEVELOPMENT_ENVIRONMENT/node"
+  dir="$dev_dir/node"
   ;;
 esac
 
-echo $dir
+# Fail early if bad language
+if [ -z "$dir" ];then
+  echo "Environment does not exist"
+  exit 1
+fi
+
+# Ensure path does not contain a file
+path=$2
+if [ -f $path ]; then
+  echo "File exists at path"
+  exit 1
+fi
+
+# Create path if it does not exist
+if [ ! -d $path ]; then
+  echo "Creating directory $path"
+  mkdir -p "$path"
+fi
+
+# Copy .devcontainer directory to target directory
+echo "Copying $language environment to $path"
+cp -r "$dir/.devcontainer" "$path"
